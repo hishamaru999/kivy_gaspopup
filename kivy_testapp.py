4e5f1b4e06c61e247c9f6513a9045e194b736069
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2021-01-17 20:55:45 -0800

    basic layout

--- kivy_testapp.py
+++ kivy_testapp.py
@@ -1,11 +1,43 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 kivy.require("1.10.1")
+
+
+# super() initiates the parent class method as well
+class ConnectPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+
+        self.add_widget(Label(text="Trader Gas:"))
+        self.traderGas = TextInput(multiline=False)
+        self.add_widget(self.traderGas)
+
+        self.add_widget(Label(text="Fast Gas:"))
+        self.fastGas = TextInput(multiline=False)
+        self.add_widget(self.fastGas)
+
+        self.add_widget(Label(text="Average Gas:"))
+        self.avgGas = TextInput(multiline=False)
+        self.add_widget(self.avgGas)
+
+        self.add_widget(Label(text="Slow Gas:"))
+        self.slowGas = TextInput(multiline=False)
+        self.add_widget(self.slowGas)
+
+        self.add_widget(Label(text="Timestampe:"))
+        self.tStamp = TextInput(multiline=False)
+        self.add_widget(self.tStamp)
+
+
+
 
 class GasApp(App):
     def build(self):
-        return Label(text="Hello!")
+        return ConnectPage()
 
 
 if __name__ == "__main__":
