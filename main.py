7af84ab7e39db1a243afee5b2cacc8cf6b880243
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2021-01-21 12:20:42 -0800

    set up gridlayout for gas app

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from kivy.clock import Clock
 import datetime
 
 
-# Format current timestamp
-date = datetime.datetime.today()
-now = date.today()
-format = "%a, %b %d %H:%M:%S"
-timeStamp = now.strftime(format)
-
-
 class MainApp(App):
     def on_start(self):
         Clock.schedule_interval(self.update_label, 1)
 
+    def get_time(self):
+        # Format current timestamp
+        date = datetime.datetime.today()
+        now = date.today()
+        format = "%a, %b %d %H:%M:%S"
+        timeStamp = now.strftime(format)
+
     def update_label(self, *args):
         # Update my label
         self.root.ids.counter.text = str(int(self.root.ids.counter.text) + 1)
+        self.root.ids.timeLabel.text = "new time"
 
 
 if __name__ == "__main__":
