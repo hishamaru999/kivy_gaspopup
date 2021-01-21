c884d38ca3a54d8fad97fe8a3cdfbadfc2f9c3f2
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2021-01-21 12:30:22 -0800

    updating time label in kivy now works on schedule

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ import datetime
 class MainApp(App):
     def on_start(self):
         Clock.schedule_interval(self.update_label, 1)
+        Clock.schedule_interval(self.update_time, 3)
 
-    def get_time(self):
-        # Format current timestamp
-        date = datetime.datetime.today()
-        now = date.today()
-        format = "%a, %b %d %H:%M:%S"
-        timeStamp = now.strftime(format)
 
     def update_label(self, *args):
         # Update my label
         self.root.ids.counter.text = str(int(self.root.ids.counter.text) + 1)
-        self.root.ids.timeLabel.text = "new time"
+
+
+    def update_time(self, *args):
+        self.root.ids.timeLabel.text = str(datetime.datetime.now())
+
 
 
 if __name__ == "__main__":
